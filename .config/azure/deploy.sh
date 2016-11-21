2a764d18972d151886e3c03986639b6001188009
Anna Epishova <nyirka@gmail.com>
2016-11-21 09:07:13 -0600

    Added reintall meteor at every deploy.

--- .config/azure/deploy.sh
+++ .config/azure/deploy.sh
@@ -115,6 +115,8 @@ if [ -d "$LOCALAPPDATA\meteor-azure" ]; then
   rm -rf "$LOCALAPPDATA\meteor-azure"
 fi
 
+rm -r "$LOCALAPPDATA\.meteor"
+
 # Install Meteor
 if [ ! -e "$LOCALAPPDATA\.meteor\meteor.bat" ]; then
   echo meteor-azure: Installing Meteor
